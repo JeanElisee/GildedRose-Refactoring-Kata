4fb554cb16b314aeb6df7db48f746dfd268b9722
Yao Jean-Elisée <37454954+JeanElisee@users.noreply.github.com>
2022-06-28 15:31:19 +0200

    Formated the output of the test case

--- Java/src/test/java/com/gildedrose/TexttestFixture.java
+++ Java/src/test/java/com/gildedrose/TexttestFixture.java
@@ -6,19 +6,19 @@ public class TexttestFixture {
     public static void main(String[] args) {
         System.out.println("OMGHAI!");
 
-        Item[] items = new Item[] {
-                new Item("+5 Dexterity Vest", 10, 20), //
-                new Item("Aged Brie", 2, 0), //
-                new Item("Elixir of the Mongoose", 5, 7), //
-                new Item("Sulfuras, Hand of Ragnaros", 0, 80), //
-                new Item("Sulfuras, Hand of Ragnaros", -1, 80),
-                new Item("Backstage passes to a TAFKAL80ETC concert", 15, 20),
-                new Item("Backstage passes to a TAFKAL80ETC concert", 10, 49),
-                new Item("Backstage passes to a TAFKAL80ETC concert", 5, 49),
-                new Item("Backstage passes to a TAFKAL80ETC concert", -1, 39),
-                // this conjured item does not work properly yet
-                new Item("Conjured Mana Cake", 3, 6),
-                new Item("Conjured Mana Cake", -1, 6) };
+        Item[] items = new Item[]{
+            new Item("+5 Dexterity Vest", 10, 20), //
+            new Item("Aged Brie", 2, 0), //
+            new Item("Elixir of the Mongoose", 5, 7), //
+            new Item("Sulfuras, Hand of Ragnaros", 0, 80), //
+            new Item("Sulfuras, Hand of Ragnaros", -1, 80),
+            new Item("Backstage passes to a TAFKAL80ETC concert", 15, 20),
+            new Item("Backstage passes to a TAFKAL80ETC concert", 10, 49),
+            new Item("Backstage passes to a TAFKAL80ETC concert", 5, 49),
+            new Item("Backstage passes to a TAFKAL80ETC concert", -1, 39),
+            // this conjured item does not work properly yet
+            new Item("Conjured Mana Cake", 3, 6),
+            new Item("Conjured Mana Cake", -1, 6)};
 
         GildedRose app = new GildedRose(items);
 
@@ -29,9 +29,9 @@ public class TexttestFixture {
 
         for (int i = 0; i < days; i++) {
             System.out.println("-------- day " + i + " --------");
-            System.out.println("name, sellIn, quality");
+            System.out.format("%-45s%-8s%-8s\n", "name", "sellIn", "quality");
             for (Item item : items) {
-                System.out.println(item);
+                System.out.format("%-45s%-8d%-8d\n", item.getName(), item.getSellIn(), item.getQuality());
             }
             System.out.println();
             app.updateQuality();
